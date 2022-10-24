96afbc8607db5ca3a023734fcbd49e767355cab5
Anderson Nunes <quatroovos@gmail.com>
2022-10-24 17:53:22 -0300

    Feitos requisitos 4 e 5 da tela de Login

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,20 @@
+import { act, screen } from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithContext from './renderWithContext';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Implementa testes na tela de Login', () => {
+  test('Testa elementos na tela', () => {
+    act(() => {
+      renderWithContext(<App />);
+    });
+
+    const inputEmail = screen.getByTestId('email-input');
+    const inputPassword = screen.getByTestId('password-input');
+    const btnEnter = screen.getByRole('button', { name: /enter/i });
+
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    expect(btnEnter).toBeInTheDocument();
+  });
 });
